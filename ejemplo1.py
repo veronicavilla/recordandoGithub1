ca7abe5f059dcdc8d9808758ee3916b2f0c101da
tiago0625 <sospina0625@outlook.com>
2023-08-15 08:06:48 -0500

    feat: variables primitivas y lista de datos

---
+++ ejemplo1.py
@@ -0,0 +1,11 @@
+#soy un comentario de linea
+nombreUsuario="Verónica Montoya"
+edadUsuario=26
+estatura=1.60
+esHinchaDeAlgunEquipo=True #esto es un comentario de bloque(''')
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
+
+comidasFavoritas=["Frijoles", "pasta", "mazamorra", "pizza"]
+print(comidasFavoritas)
+print(comidasFavoritas[2])
